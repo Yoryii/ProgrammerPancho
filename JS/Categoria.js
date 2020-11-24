d7a314625d49113c2ac86fd3030caf361e3da556
Yoryii <72758508+Yoryii@users.noreply.github.com>
2020-11-24 11:12:23 -0600

    Validación categorias

--- JS/Categoria.js
+++ JS/Categoria.js
@@ -1,1 +1,19 @@
+function validar(form){
+    var cad=validarLimite(form.limiteSemestre.value);
+    if(cad!=''){
+        document.getElementById("notificaciones").innerHTML='<p>'+cad+'</p>';
+        return false;
+    }
+    else{
+       return true; 
+    }
+}
+
+function validarLimite(limite){
+    if(limite > 8 || limite < 1){
+        return 'El semestre límite debe estar en el rango 1 - 8';
+    } else {
+        return '';
+    }
+}
 
